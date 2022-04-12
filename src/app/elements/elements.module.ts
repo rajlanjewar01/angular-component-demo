739d4cad7c3bb08d365d5d39499e223593c5ab21
rajperficient01 <rajl.perficient@gmail.com>
2022-04-12 15:22:47 +0530

    initial commit

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementHomeComponent } from './element-home/element-home.component';
+
+
+@NgModule({
+  declarations: [
+    ElementHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule
+  ],
+  exports:[]
+})
+export class ElementsModule { }
